1ec22132b2eb7dce1ab4e780f9517316844bead9
Vats Hariyani <62383230+vatshariyani@users.noreply.github.com>
2023-03-22 16:43:13 +0530

    Update internet_check.py
    Removed unwanted code and comments and removed recursion. Internet connection will only be checked when necessary.

--- internet_check.py
+++ internet_check.py
@@ -1,65 +1,14 @@
-import urllib.request
+import requests
+import sys
 
-a=(str("Checking internet connection..."))
+#function to check internet
 
-print(a)
-
-def connect(host='http://duckduckgo.com'):
-      try:
-          urllib.request.urlopen(host)
-          return True
-      except:
-          return False
-# test
-print("connected to the internet" if connect() else "You are not connected to the internet")
-
-"""import requests
-
-def url_checker(url):
-	try:
-		#Get Url
-		get = requests.get(url)
-		# if the request succeeds 
-		if get.status_code == 200:
-			return(f"{url}: is reachable")
-		else:
-			return(f"{url}: is Not reachable, status_code: {get.status_code}")
-
-	#Exception
-	except requests.exceptions.RequestException as e:
-        # print URL with Errs
-		raise SystemExit(f"{url}: is Not reachable \nErr: {e}")"""
-
-
-
-"""import speedtest
-
-# Speed test
-#st = speedtest.Speedtest()
-
-# Download Speed
-#ds = st.download()
-
-#print(ds)
-
-
-import whois
-
-domain = 'pytutorial.com'
-
-get_info = whois.whois(domain)
-
-print(get_info)
-
-"""
-
-"""import urllib.request
-def connect(host='http://google.com'):
+def check_internet(host = "https://duckduckgo.com/", timeout = 100):
     try:
-        urllib.request.urlopen(host) #Python 3.x
-        return True
-    except:
-        return False
-# test
-print( "connected" if connect() else "no internet!" )
-"""
+        requests.head(host, timeout = timeout)
+        print("You are connected to internet.")
+    except requests.ConnectionError:
+        print("Please check your internet connection.")
+
+
+check_internet()
